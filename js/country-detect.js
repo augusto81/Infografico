f639edf17339096a3181dce2e0a0850523e448b8
Fábio Rodrigues <fabio.info@gmail.com>
2015-08-27 22:55:27 -0300

    Ajustado links conforme pais

--- js/country-detect.js
+++ js/country-detect.js
@@ -1,16 +1,16 @@
 $(function() {
   var params = (function() {
 
-      var str = window.location.search;
-      var objURL = {};
+    var str = window.location.search;
+    var objURL = {};
 
-      str.replace(
-          new RegExp("([^?=&]+)(=([^&]*))?", "g"),
-          function( $0, $1, $2, $3 ){
-              objURL[ $1 ] = $3;
-          }
-      );
-      return objURL;
+    str.replace(
+      new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+      function( $0, $1, $2, $3 ){
+          objURL[ $1 ] = $3;
+      }
+    );
+    return objURL;
   })();
 
   $("body").addClass(params["pais"])
